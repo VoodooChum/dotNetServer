60999ca5bd2e0831b352d602979b520db0eb68b2
voodoochum <brian@aquavisionnola.com>
2019-06-05 16:12:57 -0500

    Added get to Student Endpoint for all Students (Mock Data)

--- Controllers/StudentController.cs
+++ Controllers/StudentController.cs
@@ -1,10 +1,20 @@
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
+using dotNetServer.Models;
 
 namespace dotNetServer.Controllers
 {
   [Route("api/[controller]")]
-  public class CatController : Controller
+  public class StudentController : Controller
   {
-
+    [HttpGet]
+    public IEnumerable<Student> GetAll()
+    {
+      var studentList = new List<Student>();
+      studentList.Add(new Student() { Name = "Billy", Age = "40", Sex = "Male" });
+      studentList.Add(new Student() { Name = "Brian", Age = "47", Sex = "Male" });
+      studentList.Add(new Student() { Name = "Peyton", Age = "19", Sex = "Female" });
+      return studentList;
+    }
   }
 }
